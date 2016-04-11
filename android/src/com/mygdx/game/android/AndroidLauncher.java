88ca4341fff3d99316f661eaa481944d7137a39e
rems51 <remi.rousseau51@hotmail.fr>
2016-04-11 15:12:47 -0400

    partie 1 du tutoriel

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -1,7 +1,6 @@
 package com.mygdx.game.android;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
@@ -11,6 +10,10 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useAccelerometer = false;
+        config.useCompass = false;
+        config.useWakelock = true;
+        config.useGL20 = true;
 		initialize(new MyGdxGame(), config);
 	}
 }
